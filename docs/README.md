109dd12040cd1ad70bce46d5b77c43eb6592b245
ConnerWill <conner.will@outlook.com>
2025-01-13 11:49:32 -0600

    Using functions and added modularity

--- docs/README.md
+++ docs/README.md
@@ -8,3 +8,7 @@
 ![arduino schematics](../static/img/arduino-water-sensor-led-wiring-diagram.jpg)
 
 ---
+
+## Tutorial
+
+[Arduino Water Level Sensor](https://arduinogetstarted.com/tutorials/arduino-water-sensor)
--- src/main/waterlevelsensor.ino
+++ src/main/waterlevelsensor.ino
@@ -1,7 +1,5 @@
 /*
  *
- * Tutorial page: https://arduinogetstarted.com/tutorials/arduino-water-sensor
- *
  *
 */
 
@@ -10,27 +8,50 @@
 #define SIGNAL_PIN  A5
 #define THRESHOLD   300
 
-int value = 0; // variable to store the sensor value
+int value = 0;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(LED_PIN,   OUTPUT);   // configure D2 pin as an OUTPUT
-  pinMode(POWER_PIN, OUTPUT);   // configure D7 pin as an OUTPUT
-  digitalWrite(POWER_PIN, LOW); // turn the sensor OFF
-  digitalWrite(LED_PIN,   LOW); // turn LED OFF
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(POWER_PIN, OUTPUT);
+  digitalWrite(POWER_PIN, LOW);
+  digitalWrite(LED_PIN, LOW);
+}
+
+void turnSensorOn() {
+  digitalWrite(POWER_PIN, HIGH);
+}
+
+void turnSensorOff() {
+  digitalWrite(POWER_PIN, LOW);
+}
+
+int getAverageReading() {
+  int total = 0;
+  const int numSamples = 10;
+  for (int i = 0; i < numSamples; i++) {
+    total += analogRead(SIGNAL_PIN);
+    delay(10);
+  }
+  return total / numSamples;
+}
+
+void checkWater() {
+  value = getAverageReading();
+  if (value > THRESHOLD) {
+    Serial.println("Water is detected");
+    digitalWrite(LED_PIN, LOW);  // Turn LED OFF when water is detected
+  } else {
+    Serial.println("Water is NOT detected");
+    digitalWrite(LED_PIN, HIGH); // Turn LED ON when no water is detected
+  }
 }
 
 void loop() {
-  digitalWrite(POWER_PIN, HIGH);  // turn the sensor ON
-  delay(10);                      // wait 10 milliseconds
-  value = analogRead(SIGNAL_PIN); // read the analog value from sensor
-  digitalWrite(POWER_PIN, LOW);   // turn the sensor OFF
-
-  if (value > THRESHOLD) {
-    Serial.print("Water is detected");
-    digitalWrite(LED_PIN, LOW);   // turn LED OFF
-  } else {
-    Serial.print("Water is NOT detected");
-    digitalWrite(LED_PIN, HIGH);  // turn LED ON
-  }
+  turnSensorOn();
+  delay(10);
+  checkWater();
+  turnSensorOff();
+  delay(500); // Add delay to reduce sensor readings frequency
 }
+
